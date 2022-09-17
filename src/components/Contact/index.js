cca62f895995f90a03f05f04c3ddd320b7e3a4cd
Anthony Pena <apena5@alumni.jh.edu>
2022-09-17 15:17:33 -0600

    feat: remove artist website

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,10 +5,6 @@ function ContactForm() {
       <div>
 
         <div>
-          <a className="contactItem" href="https://www.anthony-e-pena.com">artist website</a>
-        </div>
-
-        <div>
           <a className="contactItem" href="mailto: anthony.e.p3na@gmail.com">anthony.e.p3na@gmail.com</a>
         </div>
 
